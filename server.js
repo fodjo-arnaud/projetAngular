b0cdb8482e217ad7d4c80d83c6f06ab35d217034
1111200 <juniorfodjo415@gmail.com>
2026-03-19 02:08:11 +0000

    fix render port

--- server.js
+++ server.js
@@ -17,4 +17,6 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/auth", authRoutes);
 app.use("/api/assignments", assignmentRoutes);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
